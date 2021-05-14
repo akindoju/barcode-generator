c190b09134532511f9ecc4e2fb00724d2d588e7f
Akndju <davidd.akindoju@gmail.com>
2021-05-14 13:08:41 +0100

    disabled btn if field is empty to avoid errors and added one comment

--- src/pages/barcodePage/barcodePage.jsx
+++ src/pages/barcodePage/barcodePage.jsx
@@ -5,25 +5,23 @@ const BarcodePage = () => {
   const [barcodeValue, setBarcodeValue] = useState('');
   const [btnClicked, setBtnClicked] = useState(false);
 
-  // const displayBarcode = () => {
-  //   <Barcode barcodeValue={barcodeValue} />;
-  //   setBtnClicked(false);
-  // };
-
   return (
     <div>
       <form>
-        <label htmlFor="barcode">Enter value for Barcode</label>
+        <label htmlFor="barcode">Enter Barcode Value</label>
         <input
           id="barcode"
           onChange={(event) => {
             event.preventDefault();
             setBarcodeValue(event.target.value);
+
+            //to reset barcode value and also state
             setBtnClicked(false);
           }}
         />
       </form>
       <button
+        disabled={barcodeValue.length === 0}
         onClick={(event) => {
           event.preventDefault();
           setBtnClicked(true);
